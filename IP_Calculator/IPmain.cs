d68472c2bfafdfe14faf97b88b9fb97e2d6c2370
Newbs-dot <57189271+Newbs-dot@users.noreply.github.com>
2020-04-25 16:11:35 +0500

    added mask

---
+++ IP_Calculator/IPmain.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace IP_Calculator
+{
+    class IPmain
+    {
+        public static IP_address get_Subnet(IP_address ip, int cidr) {
+            IP_address temp;
+            byte[] bit_decimal = new byte[] { 0, 128, 192, 224, 240, 248, 252, 254, 255 };
+
+            if (cidr <= 8)
+            {
+                byte first = bit_decimal[cidr];
+                temp = new IP_address(first, 0, 0, 0);
+            }
+            else if (cidr > 8 && cidr <= 16)
+            {
+                int pwr = cidr - 8;
+                byte second = bit_decimal[pwr];
+                temp = new IP_address(255, second, 0, 0);
+            }
+            else if (cidr > 16 && cidr <= 24)
+            {
+                int pwr = cidr - 16;
+                byte third = bit_decimal[pwr];
+                temp = new IP_address(255, 255, third, 0);
+            }
+            else if (cidr > 24 && cidr <= 32)
+            {
+                int pwr = cidr - 24;
+                byte fourth = bit_decimal[pwr];
+                temp = new IP_address(255, 255, 255, fourth);
+            }
+            else {
+                temp = new IP_address(0, 0, 0, 0);
+            }
+            IP_address sub = new IP_address((byte)(temp.first_octet & ip.first_octet),
+                (byte)(temp.second_octet & ip.second_octet),
+                (byte)(temp.third_octet & ip.third_octet),
+                (byte)(temp.fourth_octet & ip.fourth_octet));
+            return sub;
+            
+        }
+        public static IP_Mask GetIP_Mask(int cidr){
+            IP_Mask mask;
+            byte[] bit_decimal = new byte[] { 0, 128, 192, 224, 240, 248, 252, 254, 255 };
+
+            if (cidr <= 8)
+            {
+                byte first = bit_decimal[cidr];
+                mask= new IP_Mask(first, 0, 0, 0);
+            }
+            else if (cidr > 8 && cidr <= 16)
+            {
+                int pwr = cidr - 8;
+                byte second = bit_decimal[pwr];
+                mask = new IP_Mask(255, second, 0, 0);
+            }
+            else if (cidr > 16 && cidr <= 24)
+            {
+                int pwr = cidr - 16;
+                byte third = bit_decimal[pwr];
+                mask = new IP_Mask(255, 255, third, 0);
+            }
+            else if (cidr > 24 && cidr <= 32)
+            {
+                int pwr = cidr - 24;
+                byte fourth = bit_decimal[pwr];
+                mask = new IP_Mask(255, 255, 255, fourth);
+            }
+            else
+            {
+                mask = new IP_Mask(0, 0, 0, 0);
+            }
+
+            return mask;
+        }
+
+    }
+    class IP_address {
+        public byte first_octet,second_octet,third_octet,fourth_octet;
+        public IP_address(byte a, byte b, byte c, byte d) {
+            first_octet = a;
+            second_octet = b;
+            third_octet = c;
+            fourth_octet = d;
+        }
+        
+    }
+    class IP_Mask {
+        public byte f_octet, s_octet, t_octet, fo_octet;
+       
+        public IP_Mask(byte a ,byte b, byte c, byte d) {
+            f_octet = a;
+            s_octet = b;
+            t_octet = c;
+            fo_octet = d;
+        }
+    
+    }
+}
---
+++ IP_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IP_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public int index = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            for (int i = 0; i < 32; i++)
+            {
+                ComboBoxItem a = new ComboBoxItem();
+                 a.Content = i.ToString();
+                CIDR.Items.Add(a);
+            }
+            
+            
+            CIDR.SelectionChanged += OnSelectionChanged;
+            CIDR.DropDownOpened += OnDropDownOpened;
+        }
+        private void OnDropDownOpened(object sender, EventArgs e)
+        {
+            CIDR.SelectedItem = null;
+        }
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            
+            if (CIDR.SelectedItem != null)
+            {
+                ComboBoxItem a = (ComboBoxItem)CIDR.SelectedItem;
+                int cidrM = int.Parse(a.Content.ToString());
+                IP_Mask mask = IPmain.GetIP_Mask(cidrM);
+                string valm = String.Format("{0}.{1}.{2}.{3}",
+                    mask.f_octet, mask.s_octet, mask.t_octet,
+                    mask.fo_octet);
+
+                Mask.Text = valm;
+               
+            }
+        }
+       
+
+    }
+}
